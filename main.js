9c4b500e1ff901dade4e5d91ed82403ba6fe1224
TechWithShreban <shreban17@gmail.com>
2022-12-17 11:35:44 +0530

    new file:   contribute.html modified:   home.html modified:   main.js modified:   style.css

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 let menu = document.querySelector("#menu-bars");
+let search = document.querySelector("#search");
 let navbar = document.querySelector(".nav_bar");
+let searchbar = document.querySelector(".search_bar");
 
 menu.onclick = () => {
     menu.classList.toggle('fa-time');
     navbar.classList.toggle('active');
+    search.classList.toggle('fa-time');
+    searchbar.classList.toggle('active');
 }
